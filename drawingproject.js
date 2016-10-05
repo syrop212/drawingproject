7a8d1935349b674f4cf794a96b123376854109bf
Carlos Casas <albertocasas05@gmail.com>
2016-10-05 15:03:21 -0700

    somethinh

--- drawingproject.js
+++ drawingproject.js
@@ -3,9 +3,7 @@ var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
 
-//background
-canvas.fillStyle = "Azure";
-canvas.fillRect(0, 0, 800, 800);
+
 
 //circle
 canvas.beginPath();
@@ -15,18 +13,10 @@ canvas.arc(398, 398, 395, 0, 2*Math.PI);
 canvas.arc(397, 397, 395, 0, 2*Math.PI);
 canvas.arc(396, 396, 395, 0, 2*Math.PI);
 canvas.arc(395, 395, 395, 0, 2*Math.PI);
+canvas.fillStyle = "black";
+canvas.closePath();
 canvas.stroke();
 
-//head
-canvas.fillStyle = "red";
-canvas.beginPath();
-canvas.moveTo(200, 100);
-canvas.lineTo(400,75);
-canvas.lineTo(400,25);
-canvas.moveTo(400, 100);
-canvas.lineTo(400,75);
-canvas.lineTo(200,25);
-canvas.fill();
 
 //neck
 canvas.beginPath();
